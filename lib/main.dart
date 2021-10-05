b4035a9e86854f58f1c72e39303a9d726f1c03e4
Dioner Weiss <dionerweiss@gmail.com>
2021-10-04 22:39:23 -0300

    add intl

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -16,6 +17,9 @@ class ExpensesApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   MyHomePage({Key? key}) : super(key: key);
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
 
   final _transactions = [
     Transaction(
@@ -39,8 +43,9 @@ class MyHomePage extends StatelessWidget {
           title: Text('Despesas Pessoais'),
         ),
         body: Column(
-          children: const [
-            SizedBox(
+          // crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
@@ -48,10 +53,90 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
+            Column(
+              children: _transactions
+                  .map(
+                    (tr) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            margin: const EdgeInsets.symmetric(
+                              horizontal: 15,
+                              vertical: 10,
+                            ),
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                              color: Colors.purple,
+                              width: 2,
+                            )),
+                            padding: const EdgeInsets.all(10),
+                            child: Text(
+                              'R\$ ${tr.value}',
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                tr.title,
+                                style: const TextStyle(
+                                    fontSize: 16, fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                DateFormat('d MMM y').format(tr.date),
+                                style: const TextStyle(
+                                  color: Colors.grey,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
             Card(
-              child: Text('Lista de Transações'),
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    TextField(
+                      controller: titleController,
+                      decoration: const InputDecoration(labelText: 'Título'),
+                    ),
+                    TextField(
+                      controller: valueController,
+                      decoration: const InputDecoration(
+                        labelText: 'Valor (R\$)',
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        TextButton(
+                          style: TextButton.styleFrom(primary: Colors.purple),
+                          child: const Text('Nova Transação'),
+                          onPressed: () {
+                            print(titleController.text);
+                            print(valueController.text);
+                          },
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             )
           ],
         ));
   }
 }
+
+class Value {}
