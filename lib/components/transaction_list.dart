9a7549e4812047f8ae028c413600c22ddd21a167
Dioner Weiss <dionerweiss@gmail.com>
2021-10-13 18:06:42 -0300

    delete transaction

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -3,9 +3,12 @@ import 'package:intl/intl.dart';
 import '../models/transaction.dart';
 
 class TransactionList extends StatelessWidget {
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
+  const TransactionList(this.transactions, this.onRemove, {Key? key})
+      : super(key: key);
 
   final List<Transaction> transactions;
+
+  final void Function(String id) onRemove;
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +56,11 @@ class TransactionList extends StatelessWidget {
                         style: Theme.of(context).textTheme.headline6),
                     subtitle: Text(
                       DateFormat('d MMM y').format(tr.date),
+                    ),
+                    trailing: IconButton(
+                      icon: const Icon(Icons.delete),
+                      color: Theme.of(context).errorColor,
+                      onPressed: () => onRemove(tr.id),
                     ),
                   ),
                 );
